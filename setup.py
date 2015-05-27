57c5a19eef4ad18fcb18b9160500a8e3fb8e35dd
Jiri Machalek <jiri.machalek@tieto.com>
2015-05-27 13:06:25 +0200

    add minimal setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ontap-api-wrapper',
+      version='0.5',
+      description='Python wrapper for NetApp Manageability SDK',
+      author='Andrew Leonard',
+      author_email='andy.leonard@sbri.org',
+      license='Apache License',
+      py_modules=['Ontap'],
+     )
+
